89c425479894022dda2dc6d92ebb87031abb9160
unknown <emiliojoaquinortizmalich@gmail.com>
2024-05-03 09:51:44 -0300

    Cree el archivo assets, index.html y tambien rangoEdad.js

---
+++ assets/js/ragoEdad.js
@@ -0,0 +1,20 @@
+let edad = parseFloat(prompt("Ingrese una edad"))
+
+if (edad >0 && edad <=12){
+    console.log("Niño")
+}
+else if(edad >13 && edad<19){
+    console.log("Adolescente")
+}
+else if(edad >20 && edad <59){
+    console.log("Adulto")
+}
+else if(edad >60 && edad <=119 ){
+    console.log("Adulto mayor")
+}
+else if (edad>120 ){
+    console.log("Re muerto ya debe estar")
+}
+else if (edad !=Number){
+    console.log("No ingreso una edad")
+}

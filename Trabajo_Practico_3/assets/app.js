202d9f5b9d439280e0036644810f5fdc030e6abf
unknown <emiliojoaquinortizmalich@gmail.com>
2024-05-17 14:46:33 -0300

    falta la validacion de los espacios en blanco

--- Trabajo_Practico_3/assets/app.js
+++ Trabajo_Practico_3/assets/app.js
@@ -1,25 +1,52 @@
+function agregarTarea(nombre) {
+    const regex = /^\S+$/;
+    if (regex.test(nombre)){
 
+    tarea.push(prompt("ingrese la nueva tarea: "));
+    alert("tarea añdida \n" + tarea);
+    }else{
+        alert("No se puede ingresar espacios en blanco")
+    }
+}
+    function listaTarea() {
+        let lista="  ";
+    for( e = 0; e < tarea.length; e++){
+        lista += tarea[e] + " , ";
+        console.log(lista );
+    }
+    }
+    function editarTarea(tarea) {
+    let posicion = parseInt(prompt("ingrese la posicion de la tarea a editar"));
+    tarea[posicion] = prompt("ingrese la nueva tarea");
+    alert("tarea editada \n" + tarea);
+    }
+    function eliminarTarea(tarea) {
+    let posicion = parseInt(prompt("ingrese la posicion de la tarea a eliminar"));
+    tarea.splice(posicion, 1);
+    console.log("tarea eliminada \n" + tarea);
+    }
+let tarea=[];
 let i;
 let r=1;
 while (r !==0 ) {
-    i = parseInt(prompt(`Que quiere hacer \n
-    1. Agregar tarea \n
-    2. lista de tarea \n
-    3. editar tarea \n
-    4. eliminar tarea \n
-    0. salir \n`)); 
+    i = parseInt(prompt(`Que quiere hacer
+    1. Agregar tarea 
+    2. lista de tarea 
+    3. editar tarea 
+    4. eliminar tarea 
+    0. salir `)); 
 switch (i) {
     case 1:
-        console.log("agregar tarea");
+        agregarTarea();
         break;
     case 2:
-        console.log("lista de tarea");
+        listaTarea(tarea);
         break;
     case 3:
-        console.log("editar tarea");
+        editarTarea(tarea);
         break;
     case 4:
-        console.log("eliminar tarea");
+        eliminarTarea(tarea);
         break;
     case 0:
         console.log("salir");

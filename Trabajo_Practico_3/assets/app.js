ae8a85efbd7c57205d5c3f04cfa12da5dec0e1fe
unknown <emiliojoaquinortizmalich@gmail.com>
2024-05-20 09:08:20 -0300

    Se realizaron los cambios par que se pueda editar con el nombre y no el indice

--- Trabajo_Practico_3/assets/app.js
+++ Trabajo_Practico_3/assets/app.js
@@ -20,22 +20,29 @@ function listaTarea() {
     }
     alert(lista);
 }
-function editarTarea(tarea) {
-    let posicion = parseInt(prompt("Ingrese la posición de la tarea a editar"));
-    let nombre = prompt("Ingrese la nueva tarea");
-    const regex = /^\S+$/;
-    if (regex.test(nombre)) {
-        tarea.splice(posicion, 1, nombre);
-        alert("Tarea editada:\n" + nombre);
-    } else {
-        alert("No se puede ingresar espacios en blanco.");
-    }
+function editarTarea(nombre) {
+    let task = tarea.includes(nombre)
+    if (task == true) {
+        const regex = /^\S+$/;
+        let nuevoNom = prompt("Ingrese un nombre nuevo para la tarea:")
+        if (regex.test(nuevoNom)){
+            tarea.splice(tarea.indexOf(nombre),1,nuevoNom)
+            alert("El nombre nuevo es: "+nuevoNom)
+        } else {
+            alert("El nombre nuevo no debe contener espacios!")
+            }
+        } else {
+            alert("Nombre invalido, intente de nuevo!")
+        }
 }
 
-function eliminarTarea(tarea) {
-    let posicion = parseInt(prompt("Ingrese la posicion de la tarea a eliminar"));
-    const TAREA_ELIMINADA = tarea.splice(posicion, 1);
-    alert("Tarea eliminada \n" + TAREA_ELIMINADA);
+function eliminarTarea(nombre) {
+    let task = tarea.includes(nombre)
+    if (task == true) {
+        tarea.splice(tarea.indexOf(nombre), 1)
+    } else {
+        alert("Ese nombre no se encuentra en la lista.")
+    }
 }
 let tarea = [];
 let i;
@@ -55,10 +62,10 @@ while (r !== 0) {
             listaTarea(tarea);
             break;
         case 3:
-            editarTarea(tarea);
+            editarTarea(prompt("Ingrese el nombre de la tarea que quiere editar: "+ tarea));
             break;
         case 4:
-            eliminarTarea(tarea);
+            eliminarTarea(prompt("Ingrese el nombre del valor que quiere eliminar: "+ tarea));
             break;
         case 0:
 
